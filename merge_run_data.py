977341b8988f5cc1ea984f97472d53d1f10c8153
nstepanenko <nicole_stepanenko@comcast.net>
2025-10-28 23:31:59 -0400

    get up to date

---
+++ merge_run_data.py
@@ -0,0 +1,16 @@
+# Merging data from participation files with some run data
+
+# From participation:
+#	offense formation
+#	offense personnel
+
+# From pbp data:
+#	make sure play type is a run
+#	run location
+#	run gap
+#	yards_gained
+#	epa
+
+# Have to filter these from the data
+# Pull them out, and then merge them together into a dataframe using play_id and game_id
+#	Will probably have to use old_game_id, since participation data has no 'game_id'
